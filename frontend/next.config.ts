cac961c1f4f2dec2124dd8752bf1268eb2b80a80
nachodoria <ignaciodoriaoberman@gmail.com>
2025-11-18 22:13:45 -0500

    Frontend creations! Chat page and improved visuals

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: [],
+  },
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
